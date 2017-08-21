4609cfd76c41aa7449f634ca8bdb5ed6483ffdf8
Matt Sams <matt@mattsams.io>
2017-08-21 16:57:27 -0400

    Designing daily forecast cards.

--- app/src/main/kotlin/io/mattsams/umbrella/view/MainActivity.kt
+++ app/src/main/kotlin/io/mattsams/umbrella/view/MainActivity.kt
@@ -2,12 +2,27 @@ package io.mattsams.umbrella.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import io.mattsams.umbrella.R
 import io.mattsams.umbrella.binding.bindView
 import io.mattsams.umbrella.view.conditions.ConditionsWidget
 
 class MainActivity : AppCompatActivity() {
     private val conditionsWidget by bindView<ConditionsWidget>(R.id.conditions_widget)
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.action_settings -> TODO()
+            else -> return false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
