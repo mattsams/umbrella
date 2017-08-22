c92a9f523a524ebf15c54506bc0dfd086499d812
Matt Sams <matt@thedeadpixelsociety.com>
2017-08-22 01:36:29 -0400

    Fixed hourly cell alignment issue due to small icon.

--- app/src/main/kotlin/io/mattsams/umbrella/view/forecast/ForecastSpacingDecoration.kt
+++ app/src/main/kotlin/io/mattsams/umbrella/view/forecast/ForecastSpacingDecoration.kt
@@ -10,5 +10,6 @@ class ForecastSpacingDecoration(private val space: Int, private val spanCount: I
     override fun getItemOffsets(outRect: Rect?, view: View?, parent: RecyclerView?, state: RecyclerView.State?) {
         if (parent == null || outRect == null) return
         if (parent.getChildLayoutPosition(view) >= spanCount) outRect.top = space
+        else outRect.top = 0
     }
 }
